8b1dc265673379d93cba27b71c712bf95049b591
ernesto_foxsystem@hotmail.com <ernesto_foxsystem@hotmail.com>
2022-03-22 17:12:00 -0400

    ACTUALIZACION

--- ServiceInventario/Interfaces/IMovTransito.cs
+++ ServiceInventario/Interfaces/IMovTransito.cs
@@ -12,6 +12,12 @@ namespace ServiceInventario.Interfaces
     {
 
         DtoLib.ResultadoId Transito_Movimiento_Agregar(DtoLibInventario.Transito.Movimiento.Agregar.Ficha ficha);
+        DtoLib.ResultadoEntidad<DtoLibInventario.Transito.Movimiento.Entidad.Ficha>
+            Transito_Movimiento_GetById(int idMov);
+        DtoLib.ResultadoLista<DtoLibInventario.Transito.Movimiento.Lista.Ficha>
+            Transito_Movimiento_GetLista(DtoLibInventario.Transito.Movimiento.Lista.Filtro filtro);
+        DtoLib.Resultado Transito_Movimiento_AnularById(int idMov);
+        DtoLib.ResultadoEntidad<int> Transito_Movimiento_GetCnt(DtoLibInventario.Transito.Movimiento.Lista.Filtro filtro);
 
     }
 
